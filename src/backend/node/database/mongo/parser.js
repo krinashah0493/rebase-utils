91e7b88f4044da4676d9ae7ebafdb5dc2318b031
Aomkar <aomkar.jagdale.rebase@gmail.com>
2021-02-16 14:42:26 +0530

    parser changes added

--- src/backend/node/database/mongo/parser.js
+++ src/backend/node/database/mongo/parser.js
@@ -1,19 +1,44 @@
-const parser = (data) => {
+const _ =require("underscore")
+
+const parser = (data, type) => {
   const backEndCollection = {};
-  data.forEach((collection) => {
-    for (let [key, value] of Object.entries(collection)) {
-      if (value.backEnd) {
-        backEndCollection[key] = value.backEnd;
-
-
-      } else if (!value.backEnd) {
-        backEndCollection[key] = value;
-      } else {
-        return false;
+  if (type === "BackEnd") {
+    data.forEach((collection) => {
+      for (let [key, value] of Object.entries(collection)) {
+        if (value.backEnd) {
+          backEndCollection[key] = value.backEnd;
+        } else if (!value.backEnd) {
+          backEndCollection[key] = value;
+        } else {
+          return false;
+        }
       }
-    }
-  });
-  return backEndCollection;
+    });
+    return [backEndCollection];
+  } else if (type === "FrontEnd") {
+    let frontEndData=[]
+    let NewFrontEndCollection = [];
+    let index = 0
+    for(let i=0;i<data.length;i++){
+      if(i>index){
+        let frontEndCollection={}
+        const collectionName=data[i-1].collection_name
+        frontEndCollection[collectionName]=NewFrontEndCollection
+        frontEndData.push(frontEndCollection)
+        NewFrontEndCollection=[]
+        index+=1
+      }
+      Object.keys(data[i]).forEach(element=>{
+        console.log(_.size(data[i]))
+        if(data[i][element].frontEnd){
+          if(data[i][element].frontEnd.form){
+            NewFrontEndCollection.push(data[i][element].frontEnd.form)
+          }
+        }
+      })
+    }    
+    return frontEndData;
+  }
 };
 
-module.exports=parser
+module.exports = parser;
